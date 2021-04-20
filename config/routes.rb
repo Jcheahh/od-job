14f776ab5a6a551eff596e4e59edf1ba425341e0
jcheahh <jackcheah2@gmail.com>
2021-04-20 23:47:12 +0800

    Add claims route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "claim/show"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :jobs
+  resources :claims
 
   root "application#home"
 end
