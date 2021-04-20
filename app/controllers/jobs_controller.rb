dc908a9b4662b5963f9dd541add8148dfc35d4bb
jcheahh <jackcheah2@gmail.com>
2021-04-20 23:42:46 +0800

    Add job controller and add validates title in job model

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,31 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_job, only: [:show]
 
   def index
     @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def show; end
+
+  def create
+    @job = current_user.jobs.create!(job_params)
+
+    redirect_to jobs_path
+  end
+
+  private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description, :pick_up_address, :drop_off_address, :pick_up_latitude, :pick_up_longtitude, :drop_off_latitude,
+                                :drop_off_longtitude)
   end
 end
