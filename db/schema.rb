f76a4e353bccdb7a7c353fc31f1a3272849097e6
jcheahh <jackcheah2@gmail.com>
2021-04-23 00:22:28 +0800

    Change claim job_id to unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_082359) do
+ActiveRecord::Schema.define(version: 2021_04_22_145122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
