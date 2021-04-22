74fcff38380b6687b136050994b9cc18f638e494
jcheahh <jackcheah2@gmail.com>
2021-04-22 19:15:20 +0800

    Add destroy to jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_job, only: [:show]
+  before_action :set_job, only: [:show, :destroy]
 
   def index
     @jobs = Job.all
@@ -16,6 +16,19 @@ class JobsController < ApplicationController
     @job = current_user.jobs.create!(job_params)
 
     redirect_to jobs_path
+  end
+
+  def destroy
+    @job = current_user.jobs.find(params[:id])
+    if @job.claim.nil?
+      @job.destroy
+
+      redirect_to jobs_path
+    else
+      flash[:danger] = "This job is claimed"
+
+      redirect_to @job
+    end
   end
 
   private
