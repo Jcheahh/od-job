f11060962a05f891befa4d1e9fc1ba829aa56e8f
jcheahh <jackcheah2@gmail.com>
2021-04-23 23:52:05 +0800

    Update validates and add geocoded_by to check jobs location

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,10 @@ class Job < ApplicationRecord
   belongs_to :user
   has_one :claim
 
-  validates_presence_of :title
+  geocoded_by :pick_up_address, latitude: :pick_up_latitude, longitude: :pick_up_longitude
+
+  # after_validation :geocode
+
+  validates_presence_of :title, :description, :pick_up_address, :drop_off_address, :pick_up_latitude, :pick_up_longitude, :drop_off_latitude,
+                        :drop_off_longitude
 end
