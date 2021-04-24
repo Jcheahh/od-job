0d514f26f876cc69572cb8be9a6d7b45db199242
jcheahh <jackcheah2@gmail.com>
2021-04-24 18:46:32 +0800

    Add mine endpoint to routes.rb and in index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :jobs
+  resources :jobs do
+    collection do
+      get "mine"
+    end
+  end
   resources :claims
 
   root "application#home"
