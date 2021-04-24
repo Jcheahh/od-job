e367b1b3e055643dfcbde866d200d750df56fdc9
jcheahh <jackcheah2@gmail.com>
2021-04-24 23:06:11 +0800

    Add rspec for test

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,8 +4,6 @@ class Job < ApplicationRecord
 
   geocoded_by :pick_up_address, latitude: :pick_up_latitude, longitude: :pick_up_longitude
 
-  # after_validation :geocode
-
   validates_presence_of :title, :description, :pick_up_address, :drop_off_address, :pick_up_latitude, :pick_up_longitude, :drop_off_latitude,
                         :drop_off_longitude
 end
