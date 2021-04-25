018d05ef24ecab15193a7b41d8041f6ee335cbf7
jcheahh <jackcheah2@gmail.com>
2021-04-25 17:13:39 +0800

    Update rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,12 +24,6 @@ require "rspec/rails"
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -43,16 +37,11 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
-  end
-
-  # start the transaction strategy as examples are run
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
   end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
@@ -82,4 +71,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  # config.include Devise::Test::ControllerHelpers, type: :request
+
+  # config.include Warden::Test::Helpers
 end
