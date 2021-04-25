883f7bd6f58495767e98547669f59c9e7961e319
jcheahh <jackcheah2@gmail.com>
2021-04-25 21:51:40 +0800

    Finish jobs request test case

--- spec/requests/jobs_spec.rb
+++ spec/requests/jobs_spec.rb
@@ -35,22 +35,96 @@ RSpec.describe "Jobs" do
 
     context "when the job doesn't exists" do
       let(:job_id) { 0 }
+      it "return status code 404" do
+        expect(response).to have_http_status(404)
+      end
+
       it "return cannot find" do
-        expect(response.body).to match(/Couldn't find Job/)
+        expect(response.body).to match(/The page you were looking for doesn't exist/)
       end
     end
   end
 
-  describe "POST /jobs" do
-    before do
-      sign_in user
-      post "/jobs",
-           params: { title: "example", description: "example", pick_up_address: "example", pick_up_latitude: 1,
-                     pick_up_longitude: 1, drop_off_address: "example1", drop_off_latitude: 2, drop_off_longitude: 2 }
+  describe "POST /jobs#create" do
+    let(:valid_attributes) do
+      { job: { title: "example", description: "example", pick_up_address: "example", pick_up_latitude: 1,
+               pick_up_longitude: 1, drop_off_address: "example1", drop_off_latitude: 2, drop_off_longitude: 2 } }
     end
     context "when the job is valid" do
+      before do
+        sign_in user
+        post "/jobs",
+             params: valid_attributes
+      end
       it "return created job" do
-        expect(response.body.title).to eq("example")
+        expect(response.body).not_to be_empty
+      end
+    end
+    context "when the job is invalid" do
+      before do
+        sign_in user
+        post "/jobs",
+             params: { job: { title: "example" } }
+      end
+      it "return validate fail" do
+        expect(flash[:danger]).to be_present
+      end
+
+      it "return status code 422" do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe "GET /jobs#mine" do
+    before do
+      sign_in user
+      get "/jobs/mine"
+    end
+    context "when the user claimed a job" do
+      it "return the user's job" do
+        expect(response.body).not_to be_empty
+      end
+
+      it "return status code 200" do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe "DESTROY /jobs#destroy" do
+    context "when the job is not claimed" do
+      before do
+        sign_in user
+        delete "/jobs/#{job_id}"
+      end
+      it " return status code 302" do
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context "when the job is claimed" do
+      it "return status code 405" do
+        job.create_claim(user_id: user.id)
+        sign_in user
+        delete "/jobs/#{job_id}"
+        expect(response).to have_http_status(405)
+      end
+      it "return validate fail" do
+        job.create_claim(user_id: user.id)
+        sign_in user
+        delete "/jobs/#{job_id}"
+        expect(flash[:danger]).to be_present
+      end
+    end
+
+    context "when the job is not create by yourself" do
+      let(:anotheruser) { create(:user) }
+      it "return status code 404" do
+        job.create_claim(user_id: user.id)
+        sign_in anotheruser
+        delete "/jobs/#{job_id}"
+        expect(response).to have_http_status(404)
       end
     end
   end
