aeab9a68d13156acb9ee66ab55244774ecfcb76a
jcheahh <jackcheah2@gmail.com>
2021-04-25 17:13:18 +0800

    Finish user model test case

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  subject { described_class.new(username: "Jack", email: "example@example.com", password: "password") }
+  it "is valid when User Model is valid" do
+    expect(subject).to be_valid
+  end
+
+  it "it not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "it not valid without password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+end
